93cb1bfffd9e5f59e1b5df6018fd9704269cedb9
Burxoniddin000 <sotvoldiyevburxoniddin5@gmail.com>
2023-04-18 15:32:15 +0500

    push

--- src/companet/mayin.jsx
+++ src/companet/mayin.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import man from "../img/ozim1.jpg";
-import '../scss/mani.scss'
-import download from '../img/Resume.pdf'
+import "../scss/mani.scss";
+import download from "../img/Resume.pdf";
 
 export const ComMayin = () => {
   return (
@@ -10,9 +10,11 @@ export const ComMayin = () => {
         <h1 className="resume__name">
           Hello, my name is Burhoniddin, I am Frotent Developer
         </h1>
-        <a href={download} download className="resum__dawlont">
-          <p> Download Resume</p>
-        </a>
+        <div className="resum__dawlont">
+          <a href={download} download>
+            Download Resume
+          </a>
+        </div>
       </div>
       <div>
         <img src={man} alt="man" className="resume__img" />
